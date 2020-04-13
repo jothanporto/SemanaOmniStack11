214dfc325ec120c72675084f2bfd338a720dc2c1
jothanporto <59832739+jothanporto@users.noreply.github.com>
2020-04-13 16:04:08 -0300

    mudar cor teste

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -54,7 +54,7 @@ export default function Incidents() {
           onPress={()=>{}}
           >
            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-          <Feather name="arrow-right" size={16} color="#E02041" />
+          <Feather name="arrow-right" size={16} color="#f988" />
           </TouchableOpacity>
 
         </View>
